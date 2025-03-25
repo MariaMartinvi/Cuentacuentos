8d4cd4f5c7dd0ac7dda7ee048428d734fbb8f3d6
Maria <mariamartinvillaro@gmail.com>
2025-03-25 21:22:23 +0100

    Temas de CSS

--- src/components/StoryForm.js
+++ src/components/StoryForm.js
@@ -47,12 +47,12 @@ function StoryForm({ onStoryGenerated }) {
   };
 
   return (
-    <div className="story-form">
+    <div className="story-form-container">
       <h2>
         <span className="icon-title">🦉</span>
         Mi Cuenta Cuentos
       </h2>
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit} className="story-form">
         <div className="form-group">
           <label htmlFor="topic">
             <span className="form-icon">📝</span> Introduce un tema
